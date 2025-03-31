258b91cc859f2ae272b84543a39cefc390f75aac
Dachi Bzishvili <bzishvilidachi1@gmail.com>
2025-03-31 14:50:15 +0400

    Implement React Query mutations for CRUD operations

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -11,8 +11,6 @@ type Props = {
     openForm: (id: string) => void;
     closeForm: () => void;
     editMode: boolean
-    submitForm: (activity: Activity) => void;
-    deleteActivity: (id: string) => void;
 }
 
 export default function ActivityDashboard({ activities,
@@ -22,8 +20,7 @@ export default function ActivityDashboard({ activities,
     openForm,
     closeForm,
     editMode,
-    submitForm, 
-    deleteActivity }: Props) {
+}: Props) {
 
     return (
         <Grid2 container spacing={3}>
@@ -31,21 +28,19 @@ export default function ActivityDashboard({ activities,
                 <ActivityList
                     activities={activities}
                     selectActivity={selectActivity}
-                    deleteActivity = {deleteActivity}
                 />
             </Grid2>
             <Grid2 size={5}>
                 {selectedActivity && !editMode &&
                     <ActivityDetails
-                        activity={selectedActivity}
+                        selectedActivity={selectedActivity}
                         cancelSelectActivity={cancelSelectActivity}
                         openForm={openForm}
                     />}
                 {editMode &&
                     <ActivityForm
-                        closeForm={closeForm} 
+                        closeForm={closeForm}
                         activity={selectedActivity}
-                        submitForm={submitForm}
                     />}
 
 
